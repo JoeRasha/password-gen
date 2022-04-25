01d03ef240d0e274f413fa007e163a21a0fa4cbb
joerasha <joerasha@Josephs-MacBook-Pro.local>
2022-04-24 21:03:36 -0400

    js added password gen

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
 
@@ -14,24 +15,55 @@ function generatePassword() {
 
   // prompt user for password length
   let userLength = prompt("Enter a password length between 8 and 128.");
- 
+  
   // If not a number between 8 and 128, alert user
-  // Works to return the user to the main page, so they have to click Generate Password again.
+  // This works to return the user to the main page, so they have to click Generate Password again.
   if (userLength < 8 || userLength > 128 || isNaN(userLength)) {
     alert("Please enter a valid selection for password length. Click Generate Password to try again.");
     pwString = "";
     return;
   };
 
-//The prompt user for password characters
-let userLowerCase = confirm("Would you like LOWER CASE letters in your password? Click OK for YES and Cancel for NO.");
-let userUpperCase = confirm("Would you like UPPER CASE letters in your password? Click OK for YES and Cancel for NO.");
-let userNumbers = confirm("Would you like NUMBERS in your password? Click OK for YES and Cancel for NO.");
-let userSymbols = confirm("Would you like SYMBOLS in your password? Click OK for YES and Cancel for NO.");
+  //prompt user for password characters
+  let userLowerCase = confirm("Would you like LOWER CASE letters in your password? Click OK for YES and Cancel for NO.");
+  let userUpperCase = confirm("Would you like UPPER CASE letters in your password? Click OK for YES and Cancel for NO.");
+  let userNumbers = confirm("Would you like NUMBERS in your password? Click OK for YES and Cancel for NO.");
+  let userSymbols = confirm("Would you like SYMBOLS in your password? Click OK for YES and Cancel for NO.");
 
-// The Strings for password
-let lowerCase = "abcdefghijklmnopqrstuvwxyz";
-let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let numbers = "0123456789";
-let symbols = "!*$%&#@+?:;()^";
-let pwString = "";
+  // Strings for password
+  let lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let numbers = "0123456789";
+  let symbols = "!*$%&#@+?:;()^";
+  let pwString = "";
+
+  // Create pwString based on user inputs
+  if (userLowerCase) {
+    pwString = pwString + lowerCase;
+  }
+
+  if (userUpperCase) {
+    pwString = pwString + upperCase;
+  } 
+
+  if (userNumbers) {
+    pwString = pwString + numbers;
+  }
+
+  if (userSymbols) {
+    pwString = pwString + symbols;
+  }
+
+  // if pwString has no characters, alert the user
+  if (pwString === "") {
+    alert("A password requires some characters. Click Generate Password to try again.");
+    pwString = "";
+    return;
+  }
+
+  // generate random string from StackOverflow: 
+  // https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+  let userPassword = "";
+  for (let i = 0; i < userLength; i++) {
+    userPassword += pwString.charAt(Math.floor(Math.random() * pwString.length));
+  }
